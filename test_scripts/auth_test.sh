08855c23662b5d31a275ba46060039bd2ac5f9eb
Aaron Erhardt <aaron.erhardt@t-online.de>
2020-10-29 14:16:26 +0100

    Improved auth_test.sh script

--- test_scripts/auth_test.sh
+++ test_scripts/auth_test.sh
@@ -2,31 +2,53 @@
 
 ADDR="http://127.0.0.1:8080"
 DEFAULT_HEADERS="Content-Type: application/json"
+USER_NAME="http_tester"
+PASSWORD="http_tester"
+EMAIL="tester@http.com"
 
-curl -H "$DEFAULT_HEADERS" \
+
+# Try signing up a new user
+SIGN_UP=`curl -H "$DEFAULT_HEADERS" \
   -X POST \
-  -d '{"user_name":"http_tester","password":"http_tester", "email": "tester@http.com"}' \
+  -d "{\"user_name\":\"$USER_NAME\",\"password\":\"$PASSWORD\", \"email\": \"$EMAIL\"}" \
   -i -s \
-  "$ADDR/sign_up"
+  "$ADDR/sign_up"`
+
+echo "SIGN UP:"
+echo "$SIGN_UP"
 
 echo
+echo " --- "
 echo
 
-JWT=`curl -H "$DEFAULT_HEADERS" \
+
+# Try signing the user in
+SIGN_IN=`curl -H "$DEFAULT_HEADERS" \
   -X POST \
-  -d '{"user_name":"http_tester","password":"http_tester"}' \
+  -d "{\"user_name\":\"$USER_NAME\",\"password\":\"$PASSWORD\"}" \
   -s \
   "$ADDR/sign_in"`
 
-[ -z "$JWT" ] && echo "couldn't log in!" && exit 0
+echo "SIGN IN:"
+echo "$SIGN_IN"
 
-echo "JWT: $JWT"
+[ -z "$SIGN_IN" ] && echo "couldn't log in!" && exit 0
+
+JWT="$SIGN_IN"
+
+echo
+echo " --- "
 echo
 
-curl -H "$DEFAULT_HEADERS" \
+
+# Request user information
+USER_INFO=`curl -H "$DEFAULT_HEADERS" \
   -H "Triox-JWT: $JWT" \
   -X GET \
   -i -s \
-  "$ADDR/user_info"
+  "$ADDR/user_info"`
 
- echo
+echo "USER INFO:"
+echo "$USER_INFO"
+
+echo
